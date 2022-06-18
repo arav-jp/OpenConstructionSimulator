8f00047b555e37bc76fc476481cb1fea89608b52
Autumn60 <r117159@g.kobe-kosen.ac.jp>
2022-06-18 14:00:54 +0900

    Update SceneControl.cs to call RestoreTerrain function

--- Assets/OcsSystem/Scripts/UI/SceneControl.cs
+++ Assets/OcsSystem/Scripts/UI/SceneControl.cs
@@ -8,17 +8,23 @@ namespace Ocs.GameSystem{
     public class SceneControl : MonoBehaviour
     {
         [SerializeField] private string _sceneName;
+
+        [Header("-No need to input-")]
+        [SerializeField] private DeformableTerrain _terrain;
+
         //[SerializeField] private Color loadToColor = Color.black;
         //[SerializeField] private float fadeRate = 1.0f;
 
         public void SwitchScene()
         {
+            _terrain.RestoreTerrain();
             //Initiate.Fade(_sceneName, loadToColor, fadeRate);
             SceneManager.LoadScene(this._sceneName);
         }
 
         public void StageTransition()
         {
+            _terrain.RestoreTerrain();
             SceneManager.LoadScene(GameSetting.setting.stage);
         }
 
